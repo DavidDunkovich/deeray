2377907185a07a55d2b4367571c3560790b2f01b
DavidDunkovich <dunkovichd@yahoo.com>
2018-10-26 16:22:42 -0500

    card flip set up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Pagination } from 'semantic-ui-react'
+import './App.css'
+import FlipPage from 'react-flip-page';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Pagination defaultActivePage={5} totalPages={10} />
+      <div className='flipContainer'>
+        <FlipPage orientation='horizontal' showSwipeHint='true' 
+          width='600' uncutPages='true' className='flipBook'>
+          <article>
+            <h1>My awesome first article</h1>
+            <p>My awesome first content</p>
+          </article>
+          <article>
+            <h1>My wonderful second article</h1>
+            <p>My wonderful second content</p>
+          </article>
+          <article>
+            <h1>My excellent third article</h1>
+            <p>My excellent third content</p>
+          </article>
+        </FlipPage>     
       </div>
     );
   }
